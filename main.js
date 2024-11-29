3fdd4fecd842d36558f80309dfa3d9680d8061f0
fahdkhodair <khodairfahd@gmail.com>
2024-11-29 12:42:34 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var A=document.getElementById("login");
+var B=document.getElementById("register");
+var C=document.getElementById("btn");
+function login(){
+  A.style.left="-400px";
+  B.style.left="50px";
+  C.style.left="110px";
+}
+function register(){
+  A.style.left="50px";
+  B.style.left="450px";
+  C.style.left="0";
+}
